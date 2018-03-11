ce2948b0de4fa22c6ee1b47fbb7cf6c85788dd8b
Pam Pelkola <pelkola.pam@gmail.com>
2018-03-11 17:31:26 -0500

    updated win lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,39 +5,33 @@ $(document).ready(function () {
     var wins = 0;
     var losses = 0;
     var challengeNumber = Math.floor(Math.random() * 120) + 19;
-   
-
-
+    
 
     //functions
     //use function to generate a random challenge number and dump into random-number div
 
     $("#random-button").on("click", function () {
-       
         $("#random-number").text(challengeNumber);
-    });
+    })
     //use function onclick generate random number 1-12 crystal data 
-
     $("#crystal").on("click", function () {
         var crystalNumber = Math.floor(Math.random() * 12) + 1;
         // then dump the random number into div
         $("#user-number").text(crystalNumber);
         //continuously add random numbers on click
-        console.log(crystalNumber)
-       crystalNumber = parseInt(crystalNumber);
-       counter += crystalNumber;
-       
+         counter += parseInt(crystalNumber);
+         $("#user-number").text(counter);
+         //return a win/lose message
+         if (counter === challengeNumber){
+            alert("you win!");
+            wins++;
+            
+        } else if (counter >= challengeNumber) {
+            alert("you lose");
+            losses++;
+           // ("#losses").innerhtml(losses);
+        //reset values
+        }
+        
+    }) ;
     });
-    if(counter === challengeNumber){
-        alert("you win!");
-        wins++;
-    }
-    else if(counter >= challengeNumber){
-        alert("you lose");
-        losses++
-    }
-});
-        
-        
-       
-//reset values
